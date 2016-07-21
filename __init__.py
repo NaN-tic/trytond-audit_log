99abac95e15ade2e9d544ff34ff31c8a43c993a3
Bernat Brunet Torruella <bernat@nan-tic.com>
2016-07-21 16:09:44 +0200

    Change audit log calculate and use. Now it'sa wizard with a previous filter that help to have the results faters and could print in a report too.

--- __init__.py
+++ __init__.py
@@ -7,4 +7,13 @@ from .ir import *
 def register():
     Pool.register(
         AuditLog,
+        AuditLogType,
+        OpenAuditLogStart,
+        OpenAuditLogList,
         module='audit_log', type_='model')
+    Pool.register(
+        OpenAuditLog,
+        module='audit_log', type_='wizard')
+    Pool.register(
+        AuditLogReport,
+        module='audit_log', type_='report')
--- ir.py
+++ ir.py
@@ -1,42 +1,46 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from datetime import timedelta
-from sql import Literal, Null, Cast, Union, Column
+from datetime import datetime, timedelta
+from sql import Literal, Null, Cast, Union
 from sql.operators import Concat
 from sql.functions import DateTrunc
 
 from trytond.model import ModelSQL, ModelView, fields
-from trytond.pool import Pool, PoolMeta
+from trytond.wizard import Wizard, StateView, StateAction, Button
+from trytond.pool import Pool
 from trytond.pyson import Eval
 from trytond.transaction import Transaction
+from trytond.modules.jasper_reports.jasper import JasperReport
 
-__all__ = ['AuditLog']
-__metaclass__ = PoolMeta
+__all__ = ['AuditLog', 'OpenAuditLogStart', 'OpenAuditLogList', 'OpenAuditLog',
+          'AuditLogReport', 'AuditLogType']
 
 
-class AuditLog(ModelSQL, ModelView):
-    'Audit Log'
-    __name__ = 'ir.audit.log'
-    _order = [('date', 'DESC')]
+class AuditLogType(ModelSQL, ModelView):
+    'Audit Log Type'
+    __name__ = 'ir.audit.log.type'
 
-    user = fields.Many2One('res.user', 'User')
-    date = fields.DateTime('Date & Hour')
-    type = fields.Selection([
+    name = fields.Selection([
             ('create', 'Create'),
             ('write', 'Write'),
             ('delete', 'Delete'),
             ], 'Event Type')
-    model = fields.Reference('Model', selection='models_get',
-        select=True)
+
+
+class AuditLog(ModelView):
+    'Audit Log'
+    __name__ = 'ir.audit.log'
+    _order = [('date', 'DESC')]
+
+    user = fields.Many2One('res.user', 'User', readonly=True)
+    date = fields.DateTime('Date & Hour', readonly=True)
+    type_ = fields.Many2One('ir.audit.log.type', 'Event Type', readonly=True)
+    model = fields.Many2One('ir.model', 'Model')
+    record = fields.Reference('Record', selection='models_get')
     history = fields.Boolean('History', readonly=True)
-    changes = fields.Function(fields.Text('Changes',
-            states={
-                'invisible': ~Eval('history'),
-                },
-            depends=['history']),
-        'get_changes')
-    _changes_excluded_fields = set(['id', 'create_uid', 'write_uid',
-            'create_date', 'write_date', 'rec_name'])
+    changes = fields.Text('Changes', states={
+            'invisible': ~Eval('history'),
+            }, depends=['history'])
 
     @staticmethod
     def models_get():
@@ -45,25 +49,24 @@ class AuditLog(ModelSQL, ModelView):
         return [(m.model, m.name) for m in Model.search([])]
 
     @classmethod
-    def get_common_columns(cls, table, model_name, lenght, i, history=False):
-        bigint = fields.BigInteger('Test').sql_type().base
+    def get_common_columns(cls, table, model, history=False):
         reference_type = cls.model.sql_type().base
-        id_ = Column(table, '__id') if history else table.id
-        columns = [((Cast(id_, bigint) * lenght) + i).as_('id'),
-            table.create_date.as_('create_date'),
-            table.write_date.as_('write_date'),
-            table.create_uid.as_('create_uid'),
-            table.write_uid.as_('write_uid'),
+        columns = [
             Literal(history).as_('history'),
-            Concat(model_name, Concat(',', Cast(table.id,
-                        reference_type))).as_('model'),
+            Literal(model.id).as_('model'),
+            Concat(model.model, Concat(',', Cast(table.id,
+                        reference_type))).as_('record'),
             ]
         return columns
 
     @classmethod
     def get_create_columns(cls, table):
+        pool = Pool()
+        Type = pool.get('ir.audit.log.type')
+        type_ = Type.search(['name', '=', 'create'])
+        type_ = type_ and type_[0] or None
         datetime = cls.date.sql_type().base
-        return [Literal('create').as_('type'),
+        return [Literal(type_.id).as_('type_'),
             table.create_uid.as_('user'),
             Cast(DateTrunc('seconds', table.create_date), datetime).as_(
                 'date'),
@@ -71,30 +74,46 @@ class AuditLog(ModelSQL, ModelView):
 
     @classmethod
     def get_write_columns(cls, table):
+        pool = Pool()
+        Type = pool.get('ir.audit.log.type')
+        type_ = Type.search(['name', '=', 'write'])
+        type_ = type_ and type_[0] or None
         datetime = cls.date.sql_type().base
-        return [Literal('write').as_('type'),
+        return [Literal(type_.id).as_('type_'),
             table.write_uid.as_('user'),
             Cast(DateTrunc('seconds', table.write_date), datetime).as_('date'),
             ]
 
     @classmethod
     def get_delete_columns(cls, table):
+        pool = Pool()
+        Type = pool.get('ir.audit.log.type')
+        type_ = Type.search(['name', '=', 'delete'])
+        type_ = type_ and type_[0] or None
         datetime = cls.date.sql_type().base
-        return [Literal('delete').as_('type'),
+        return [Literal(type_.id).as_('type_'),
             table.write_uid.as_('user'),
             Cast(DateTrunc('seconds', table.write_date), datetime).as_('date'),
             ]
 
     @classmethod
-    def table_query(cls):
+    def get_logs(cls, start):
         pool = Pool()
+        Type = pool.get('ir.audit.log.type')
         Model = pool.get('ir.model')
+        cursor = Transaction().cursor
+
+        types = []
+        domain = []
         queries = []
-        models = Model.search([])
-        queries_per_table = 3
-        lenght = len(models) * queries_per_table
-        for i, model in enumerate(models):
-            i = i * queries_per_table
+        for t in start.types:
+            types.append(t.name)
+
+        if start.models:
+            domain.append(
+                ('id', 'in', [m.id for m in start.models])
+                )
+        for model in Model.search(domain):
             if model.model == cls.__name__:
                 continue
             try:
@@ -104,56 +123,109 @@ class AuditLog(ModelSQL, ModelView):
                 continue
             if not issubclass(Class, ModelSQL) or Class.table_query():
                 continue
-            table = Class.__table__()
-            if Class._history:
-                table = Class.__table_history__()
-            columns = cls.get_common_columns(table, model.model, lenght, i,
-                Class._history)
-            queries.append(table.select(*(columns +
-                        cls.get_create_columns(table)),
-                    where=table.write_date == Null))
-            if Class._history:
-                table = Class.__table_history__()
-                columns = cls.get_common_columns(table, model.model, lenght,
-                    i + 1, True)
-                queries.append(table.select(*(columns +
-                            cls.get_write_columns(table)),
-                        where=table.write_date != Null))
-                table = Class.__table_history__()
-                columns = cls.get_common_columns(table, model.model, lenght,
-                    i + 2, True)
-                queries.append(table.select(*(columns +
-                            cls.get_delete_columns(table)),
-                        where=table.create_date == Null))
-            else:
-                table = Class.__table__()
-                columns = cls.get_common_columns(table, model.model, lenght,
-                    i + 1)
-                queries.append(table.select(*(columns +
-                            cls.get_write_columns(table)),
-                        where=table.write_date != Null))
-        sql, params = Union(*queries)
-        return Union(*queries)
 
-    def get_changes(self, name):
+            if Class._history:
+                table = Class.__table_history__()
+                columns = cls.get_common_columns(table, model, Class._history)
+                if not types or 'create' in types:
+                    where = table.write_date == Null
+                    if start.start_date:
+                        where &= table.create_date >= start.start_date
+                    if start.end_date:
+                        where &= table.create_date <= start.end_date
+                    if start.users:
+                        where &= table.create_uid.in_(
+                            [u.id for u in start.users])
+                    queries.append(table.select(*(columns +
+                                cls.get_create_columns(table)),
+                            where=where))
+
+                if not types or 'write' in types:
+                    where = table.write_date != Null
+                    if start.start_date:
+                        where &= table.write_date >= start.start_date
+                    if start.end_date:
+                        where &= table.write_date <= start.end_date
+                    if start.users:
+                        where &= table.write_uid.in_(
+                            [u.id for u in start.users])
+                    queries.append(table.select(*(columns +
+                                cls.get_write_columns(table)),
+                            where=where))
+
+                if not types or 'delete' in types:
+                    where = table.create_date == Null
+                    if start.start_date:
+                        where &= table.write_date >= start.start_date
+                    if start.end_date:
+                        where &= table.write_date <= start.end_date
+                    if start.users:
+                        where &= table.write_uid.in_(
+                            [u.id for u in start.users])
+                    queries.append(table.select(*(columns +
+                                cls.get_delete_columns(table)),
+                            where=where))
+            elif not types or 'write' in types:
+                table = Class.__table__()
+                columns = cls.get_common_columns(table, model)
+                where = table.write_date != Null
+                if start.start_date:
+                    where &= table.write_date >= start.start_date
+                if start.end_date:
+                    where &= table.write_date <= start.end_date
+                if start.users:
+                    where &= table.write_uid.in_([u.id for u in start.users])
+                queries.append(table.select(*(columns +
+                            cls.get_write_columns(table)),
+                        where=where))
+        if not queries:
+            return [{}]
+        sql, values = Union(*queries)
+        result = []
+        keys = ['history', 'model', 'record', 'type_', 'user', 'date']
+        cursor.execute(*Union(*queries))
+        for res in cursor.fetchall():
+            print "-------", res
+            audit_log = dict(zip(keys, res))
+            audit_log['changes'] = cls.get_changes(audit_log)
+            print "---", audit_log
+            result.append(audit_log)
+
+        res = []
+        if start.changes:
+            for audit_log in result:
+                if start.changes in audit_log['changes']:
+                    res.append(audit_log)
+        return start.changes and res or result
+
+    @staticmethod
+    def get_changes(res):
         pool = Pool()
         Field = pool.get('ir.model.field')
-        Class = pool.get(self.model.__name__)
-        _datetime = self.date - timedelta(microseconds=1)
+        Type = pool.get('ir.audit.log.type')
+        record = res.get('record') and res['record'].split(',') or []
+        if not record or len(record) != 2:
+            return ''
+        Model = pool.get(record[0])
+        record = Model(record[1])
+        Class = pool.get(record.__name__)
+        _datetime = res['date'] - timedelta(microseconds=1)
         changes = []
-        if self.type != 'write':
+        type_ = Type(res['type_'])
+        if type_.name != 'write':
             return ''
         with Transaction().set_context(_datetime=_datetime):
-            history_model = Class(self.model.id)
-        for field in Field.search([('model.model', '=', self.model.__name__)]):
-            if field.name in self._changes_excluded_fields:
-                continue
+            history_model = Class(record.id)
+        for field in Field.search([('model.model', '=', record.__name__)]):
             if field.ttype == 'one2many' or field.ttype == 'many2many':
                 continue
             if field.name not in Class._fields:
                 continue
-            new_value = getattr(self.model, field.name)
-            old_value = getattr(history_model, field.name)
+            try:
+                new_value = getattr(record, field.name)
+                old_value = getattr(history_model, field.name)
+            except:
+                continue
             if old_value != new_value:
                 if field.ttype == 'many2one' or field.ttype == 'reference':
                     old_value = old_value and old_value.rec_name or ''
@@ -161,3 +233,117 @@ class AuditLog(ModelSQL, ModelView):
                 changes.append('%s: %s -> %s' % (
                         field.field_description, old_value, new_value))
         return '\n'.join(changes)
+
+
+class OpenAuditLogStart(ModelView):
+    'Open Audit Log Start'
+    __name__ = 'ir.audit.log.open.start'
+
+    users = fields.Many2Many('res.user', None, None, 'Users')
+    start_date = fields.DateTime('Start Date & Hour')
+    end_date = fields.DateTime('End Date & Hour')
+    types = fields.Many2Many('ir.audit.log.type', None, None, 'Event Types')
+    models = fields.Many2Many('ir.model', None, None, 'Models')
+    changes = fields.Text('Changes')
+
+    @staticmethod
+    def default_start_date():
+        return datetime.now() - timedelta(days=5)
+
+    @staticmethod
+    def default_end_date():
+        return datetime.now()
+
+    @staticmethod
+    def default_company():
+        return Transaction().context.get('company')
+
+
+class OpenAuditLogList(ModelView):
+    'Open Audit Log Tree'
+    __name__ = 'ir.audit.log.open.list'
+
+    audit_logs = fields.One2Many('ir.audit.log', None, 'Audit Log', readonly=True)
+    output_format = fields.Selection([
+            ('pdf', 'PDF'),
+            ('xls', 'XLS'),
+            ], 'Output Format', required=True)
+
+    @classmethod
+    def list(cls, start):
+        pool = Pool()
+        AuditLog = pool.get('ir.audit.log')
+
+        audit_logs = AuditLog.get_logs(start)
+
+        return {
+            'audit_logs': audit_logs,
+            'output_format': 'pdf',
+            }
+
+
+class AuditLogReport(JasperReport):
+    __name__ = 'ir.audit.log.report'
+
+    @classmethod
+    def execute(cls, ids, data):
+        parameters = {}
+        return super(AuditLogReport, cls).execute(ids, {
+            'name': 'audit_log.audit_log_report',
+            'model': 'ir.audit.log',
+            'data_source': 'records',
+            'records': data['records'],
+            'parameters': parameters,
+            'output_format': data['output_format'],
+            })
+
+
+class OpenAuditLog(Wizard):
+    'Open Audit Log'
+    __name__ = 'ir.audit.log.open'
+
+    start = StateView('ir.audit.log.open.start',
+        'audit_log.audit_log_open_start_view_form', [
+            Button('Cancel', 'end', 'tryton-cancel'),
+            Button('Open', 'open_', 'tryton-ok', True),
+            ])
+    open_ = StateView('ir.audit.log.open.list',
+        'audit_log.audit_log_open_list_view_form', [
+            Button('Change', 'start', 'tryton-go-previous'),
+            Button('Print', 'print_', 'tryton-print'),
+            Button('Close', 'end', 'tryton-close'),
+            ])
+    print_ = StateAction('audit_log.report_audit_log')
+
+    def default_open_(self, fields):
+        pool = Pool()
+        AuditLogList = pool.get('ir.audit.log.open.list')
+        return AuditLogList.list(self.start)
+
+    def do_print_(self, action):
+        pool = Pool()
+        User = pool.get('res.user')
+        Type = pool.get('ir.audit.log.type')
+        Model = pool.get('ir.model')
+        records = []
+        for audit_log in self.open_.audit_logs:
+            records.append({
+                    'user': User(audit_log.user).name,
+                    'date': audit_log.date,
+                    'type': Type(audit_log.type_).name,
+                    'model': Model(audit_log.model).rec_name,
+                    'record': (audit_log.record and audit_log.record.rec_name
+                        or ''),
+                    'changes': audit_log.changes,
+                    'lang': User(audit_log.user).language.code,
+                    })
+
+        data = {
+            'records': records,
+            'output_format': self.open_.output_format,
+            }
+
+        return action, data
+
+    def transition_print_(self):
+        return 'end'
