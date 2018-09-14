2e6c796df17e1307b33b3209675521be4e48dd99
Raimon Esteve <raimon@nan-tic.com>
2018-09-14 11:46:57 +0200

    Upgrade to 4.9

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .ir import *
+from . import ir
 
 
 def register():
     Pool.register(
-        AuditLog,
-        AuditLogType,
-        OpenAuditLogStart,
-        OpenAuditLogList,
+        ir.AuditLog,
+        ir.AuditLogType,
+        ir.OpenAuditLogStart,
+        ir.OpenAuditLogList,
         module='audit_log', type_='model')
     Pool.register(
-        OpenAuditLog,
+        ir.OpenAuditLog,
         module='audit_log', type_='wizard')
     Pool.register(
-        AuditLogReport,
+        ir.AuditLogReport,
         module='audit_log', type_='report')
