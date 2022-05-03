219add3049c8cdd93d5f9ba3f730056f5c0d45bd
Albert Cervera i Areny <albert@nan-tic.com>
2022-05-03 15:00:19 +0200

    Remove CompanyTestMixin.

--- tests/test_module.py
+++ tests/test_module.py
@@ -6,7 +6,7 @@ from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AuditLogTestCase(CompanyTestMixin, ModuleTestCase):
+class AuditLogTestCase(ModuleTestCase):
     'Test AuditLog module'
     module = 'audit_log'
 
